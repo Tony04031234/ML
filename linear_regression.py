0ea495c36385a62a0ecaf3e29cc03eed00a620e8
TonyynoT <55374414+Tony04031234@users.noreply.github.com>
2021-05-20 20:05:18 +0930

    t

---
+++ linear_regression.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+from sklearn.datasets import load_boston
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+
+boston = load_boston()
+
+df_x = pd.DataFrame(boston.data, columns = boston.feature_names)
+
+df_y = pd.DataFrame(boston.target)
+print(df_y)
+
+model = linear_model.LinearRegression()
+
+x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size = 0.2, random_state = 4)
+
+model.fit(x_train, y_train)
+
+result = model.predict(x_test)
+
+print(result[5], y_test)
+
+
+
+
+
